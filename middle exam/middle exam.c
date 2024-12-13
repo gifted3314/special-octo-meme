3cbc3cf0c529fcbfee7e3bc9852df5b9c1b4495b
gifted3314 <t236102@st.yamagata-u.ac.jp>
2024-12-13 17:27:44 +0900

    Middle exam.c

---
+++ middle exam/middle exam.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char password[100];
+    int length = 0, large = 0, small = 0;
+
+    printf("パスワードを入力してください: ");
+    scanf("%99s", password);
+
+    while (password[length] != '\0') {
+        length++;
+    }
+
+    if (length < 8)  printf("パスワードを8文字以上にしてください。\n");
+
+    for (int i = 0; i < length; i++) {
+        if (password[i] >= 'A' && password[i] <= 'Z') large = 1;
+        if (password[i] >= 'a' && password[i] <= 'z') small = 1;
+    }
+
+    if (large == 0) printf("大文字のアルファベットを含めてください。\n");
+    if (small == 0) printf("小文字のアルファベットを含めてください。\n");
+
+    if (length >= 8 && large == 1 && small == 1 ) {
+        printf("このパスワードは強いです！\n");
+    }
+
+    return 0;
+}
